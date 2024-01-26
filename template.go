586de2a2c73fffb5edf10228c951223051217046
kovacs <mritd@linux.com>
2024-01-26 15:48:26 +0800

    feat(style): update default memory style
    update default memory style
    
    Signed-off-by: kovacs <mritd@linux.com>

--- template.go
+++ template.go
@@ -2,11 +2,11 @@ package main
 
 const (
 	defaultCPUInfoTpl  = `CPU: {{(index .CPU.InfoStats 0).ModelName}} {{index .CPU.Percent 0 | percentage}}`
-	defaultMemInfoTpl  = `MEM: {{.Mem.Stat.Used | humanizeIBytes}}`
+	defaultMemInfoTpl  = `MEM: {{.Mem.Stat.Used | humanizeBytes}}`
 	defaultLoadInfoTpl = `LOAD: {{.Load.Stat.Load1 | percentage}}`
 
 	defaultMiniCPUInfoTpl  = `C: {{index .CPU.Percent 0 | percentage}}`
-	defaultMiniMemInfoTpl  = `M: {{.Mem.Stat.Used | humanizeIBytes}}`
+	defaultMiniMemInfoTpl  = `M: {{.Mem.Stat.Used | humanizeBytes}}`
 	defaultMiniLoadInfoTpl = `L: {{.Load.Stat.Load1 | percentage}}`
 )
 
