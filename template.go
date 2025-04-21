7438a01a3f5e660022c5db231e6ae53deaa7bef3
kovacs <mritd@linux.com>
2025-04-21 22:28:03 +0800

    feat(host): update default host tpl
    update default host tpl
    
    Signed-off-by: kovacs <mritd@linux.com>

--- template.go
+++ template.go
@@ -1,13 +1,13 @@
 package main
 
 const (
-	defaultHostInfoTpl = `OS: {{.Host.OS}}/{{.Host.KernelVersion}}`
+	defaultHostInfoTpl = `HOST: {{.Host.OS}}/{{.Host.KernelVersion}}`
 	defaultCPUInfoTpl  = `CPU: {{(index .CPU.InfoStats 0).ModelName}} {{index .CPU.Percent 0 | percentage}}`
 	defaultMemInfoTpl  = `MEM: {{.Mem.Stat.Used | humanizeBytes}}`
 	defaultDiskInfoTpl = `DISK: {{.Disk.Stat.UsedPercent | percentage}}`
 	defaultLoadInfoTpl = `LOAD: {{.Load.Stat.Load1 | percentage}}`
 
-	defaultMiniHostInfoTpl = `O: {{.Host.OS}}`
+	defaultMiniHostInfoTpl = `S: {{.Host.OS}}`
 	defaultMiniCPUInfoTpl  = `C: {{index .CPU.Percent 0 | percentage}}`
 	defaultMiniDiskInfoTpl = `D: {{.Disk.Stat.UsedPercent | percentage}}`
 	defaultMiniMemInfoTpl  = `M: {{.Mem.Stat.Used | humanizeBytes}}`
